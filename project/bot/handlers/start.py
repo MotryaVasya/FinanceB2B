65772a3a924620711ad219b4d00b81e1ea36d41d
MaxBeltikov <86664345+MaxBeltikov@users.noreply.github.com>
2025-04-15 14:08:16 +0700

    TestBotStart

---
+++ project/bot/handlers/start.py
@@ -0,0 +1,6 @@
+from aiogram import types, F
+from app.bot.bot import dp
+
+@dp.message(F.text == "/start")
+async def start_handler(message: types.Message):
+    await message.answer("Привет! Я твой бот")
