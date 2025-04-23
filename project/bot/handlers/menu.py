95d9990dd79bfdc047963d1a69f41163b25027ad
MaxBeltikov <86664345+MaxBeltikov@users.noreply.github.com>
2025-04-23 23:16:27 +0700

    BackButtonPatch

--- project/bot/handlers/menu.py
+++ project/bot/handlers/menu.py
@@ -5,7 +5,9 @@ from project.bot.messages.messages import *
 from aiogram.types import Message
 from aiogram.filters import or_f
 from project.bot.states import *
+from project.bot.Save import save
 from project.bot.handlers.start import Context
+import logging
 from project.bot.keyboards.reply import (
     Money_keyboard,
     get_categories_keyboard,
@@ -16,30 +18,35 @@ router=Router()
 
 @router.message(F.text == "Помощь")
 async def help_handler(message: Message):
+    user_id = message.from_user.id
+    open("categories.txt", "w").write(str(await save.update(user_id, "MAIN_HELP")))
     try:
         await message.answer(
-            read_text,
+            help_text,
             reply_markup=await help_keyboard()
         )
     except Exception as e:
         print(f"⚠ Ошибка: {e.__class__.__name__}: {e}")
 
-@router.message(F.text=="Баланс")
+@router.message(F.text == "Баланс")
 async def cash_handler(message: Message):
     user_id = message.from_user.id
-    user_state_history.update({user_id:"BALANCE"})
-    text=(f"💫 Ваш баланс: \nУ тебя всё под контролем! 🧘‍♂️\n ")
+    text = f"💫 Ваш баланс: \nУ тебя всё под контролем! 🧘‍♂️\n"
+    open("balance.txt", "w").write(str(await save.update(user_id, "BALANCE")))
+    open("main44.txt", "w").write(str(await save.convert_to_json()))
     try:
         await message.answer(
             text,
             reply_markup=await Money_keyboard()
         )
-        
     except Exception as e:
         print(f"⚠ Ошибка: {e.__class__.__name__}: {e}")
 
+
 @router.message(F.text == "Категории")
 async def categories_handler(message: Message, state: FSMContext):
+    user_id = message.from_user.id
+    open("categories.txt", "w").write(str(await save.update(user_id, "MAIN_CATEGORY")))
     try:
         await state.set_state(Context.IN_CATEGORIES)
         await message.answer(
@@ -49,16 +56,14 @@ async def categories_handler(message: Message, state: FSMContext):
     except Exception as e:
         print(f"⚠ Ошибка: {e.__class__.__name__}: {e}")
 
-@router.message(or_f(F.text == "Транзакция",F.text=="Перейти к транзакциям"))
+@router.message(or_f(F.text == "Транзакция", F.text == "Перейти к транзакциям"))
 async def transaction_handler(message: Message, state: FSMContext):
+    user_id = message.from_user.id
+    open("transactions.txt", "w").write(str(await save.update(user_id, "MAIN_TRANSACTIONS")))
     try:
         await state.set_state(Context.IN_TRANSACTIONS)
         await message.answer(
-            "💸 Что вы хотите сделать с вашими транзакциями?  Выберите действие:\n"
-            "* ➕ Добавить\n"
-            "* ✏️ Изменить\n"
-            "* 🗑 Удалить\n"
-            "* 📋 Посмотреть список\n",
+            
             reply_markup=await get_transaction_keyboard()
         )
     except Exception as e:
